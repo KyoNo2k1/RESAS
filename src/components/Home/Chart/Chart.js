60308b39a835dbae3b8b92c046e7424357d337b3
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-01-25 23:45:07 +0700

    #4 fix change value ( bug cant render data from Api )

--- src/components/Home/Chart/Chart.js
+++ src/components/Home/Chart/Chart.js
@@ -8,12 +8,47 @@ import { Chart }            from 'react-chartjs-2'
 
 
 function ChartList({infoCity}) {
-    const initialState = []
-    const [dataDis, setDataDis] = useState(initialState)
-    const data = {
+
+    var data1 = [
+        {
+            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+        },
+        {
+            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+            label: "Asia",
+            borderColor: "#8e5ea2",
+            fill: false
+        },
+        {
+            data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+            label: "Europe",
+            borderColor: "#3cba9f",
+            fill: false
+        },
+        {
+            data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+            label: "Latin America",
+            borderColor: "#e8c3b9",
+            fill: false
+        },
+        {
+            data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+            label: "North America",
+            borderColor: "#c45850",
+            fill: false
+        }
+    ]
+
+    const [dataDis, setDataDis] = useState([])
+    var data = {
         labels: ["1980","1985","1990","1995","2000","2005","2010","2015","2020"],
-        datasets: dataDis,
+        datasets: data1,
     }
+    console.log(dataDis, dataDis[0]);
+    console.log(data1);
     const options={
         title: {
             display: true,
@@ -24,25 +59,24 @@ function ChartList({infoCity}) {
             position: "bottom"
         }
     }
-    const fetchData =() => {
-        infoCity.map(async (city) => {
+    const fetchData =(infoCityData) => {
+        var arrData = []
+        infoCityData?.map(async (city) => {
             const data = await api.fetchDataDis(city.cityCode,city.prefCode) .then((data) => data.data.result.data[0])
-            const newDataFetch =
-                {
-                    data:[data.data[0]?.value,data.data[1]?.value,data.data[2]?.value,data.data[3]?.value,data.data[4]?.value,data.data[5]?.value,data.data[6]?.value,data.data[7]?.value,data.data[8]?.value],
-                    label: city.cityName,
-                    borderColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-                    fill: false
-                }
-            setDataDis([...dataDis,newDataFetch])
-        })
+            const newDataFetch ={
+                                    data:[data.data[0]?.value,data.data[1]?.value,data.data[2]?.value,data.data[3]?.value,data.data[4]?.value,data.data[5]?.value,data.data[6]?.value,data.data[7]?.value,data.data[8]?.value],
+                                    label: city.cityName,
+                                    borderColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
+                                    fill: false
+                                }
+            arrData.push(newDataFetch)
+            })
+            console.log(arrData);
+            setDataDis(arrData);
     }
-    console.log(dataDis);
 
     useEffect(() => {
-        setDataDis(initialState)
-        console.log(dataDis);
-        fetchData()
+        fetchData(infoCity)
     },[infoCity])
     return(
         <div className="chart">
