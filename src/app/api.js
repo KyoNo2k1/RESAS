e0186ba1f02d585459cdaca31917916fa4ee6982
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-01-31 00:44:13 +0700

    add enviroment variable env

--- src/app/api.js
+++ src/app/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://opendata.resas-portal.go.jp/api/v1/',
-    // headers: {'X-API-KEY': 'ZOqN44U43Bc3qb7e34yAh6AcIHTyWiWt3hUZQJcP'}bJIqSKCpKa0FDvw5VanxtSGhgdT7Z5l9PsNidIm2
-    headers: {'X-API-KEY': 'bJIqSKCpKa0FDvw5VanxtSGhgdT7Z5l9PsNidIm2'}
+    baseURL: process.env.REACT_APP_BASE_URL,
+    headers: {'X-API-KEY': process.env.REACT_APP_API_KEY}
 })
 
 export const fetchCities = () => API.get(`/prefectures`)
