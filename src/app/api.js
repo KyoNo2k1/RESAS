b303c20bca5853af7d6b253d2080f3f933bdcf5c
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-02-12 15:29:14 +0700

    fix bug API cant call with enviroment variable

--- src/app/api.js
+++ src/app/api.js
@@ -4,7 +4,6 @@ const API = axios.create({
     baseURL: "https://opendata.resas-portal.go.jp/api/v1/",
     headers: {'X-API-KEY': "bJIqSKCpKa0FDvw5VanxtSGhgdT7Z5l9PsNidIm2"}
 })
-
 export const fetchCities = () => API.get(`/prefectures`)
 export const fetchDisWithCity = (prefCode) => API.get(`/cities?prefCode=${prefCode}`)
 export const fetchDataDis = (cityCode, prefCode) => API.get(`/population/composition/perYear?cityCode=${cityCode}&prefCode=${prefCode}`)
